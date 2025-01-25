51a4e127856d66f4e0b56496bf3d4e576104ce14
mosarhat <mosarhat.01@gmail.com>
2025-01-25 15:31:46 -0500

    NavBar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
+import Menu from './components/Menu.jsx';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -16,15 +15,8 @@ function App() {
 
   return (
     <>
+      <Menu />
       <h1>{initialData.message}</h1>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
